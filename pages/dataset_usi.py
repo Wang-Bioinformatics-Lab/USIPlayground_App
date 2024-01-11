635a87f7189134432a9f117e6e35ec8ab2b1a0f0
Mingxun Wang <mwang87@gmail.com>
2024-01-11 15:15:01 -0800

    adding a dataset usi manipulation

---
+++ pages/dataset_usi.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+
+# Write the page label
+st.set_page_config(
+    page_title="Dataset USI usage",
+)
+
+# Enter all the datasets accessions in textarea
+st.write("Enter all the datasets")
+
+dataset_strings = st.textarea("Enter all the datasets with new lines", "MSV000086450")
+
+datasets = usis_string.split("\n")
+
+# Getting all the USIs
+all_usis = []
+for dataset_accession in datasets:
+    all_files_url = "https://explorer.gnps2.org/api/datasets/{}/files".format(dataset_accession)
+    all_files = requests.get(all_files_url).json()
+
+    for file in all_files:
+        filename = file["filename"]
+
+        usi = "mzspec:{}:{}".format(dataset_accession, filename)
+
+        all_usis.append(usi)
+
+# Creating a df
+import pandas as pd
+df = pd.DataFrame(all_usis, columns=["usi"])
+
+st.write(df)
+
+
+
+
+
+
