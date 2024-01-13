0e68063d9b167eef322272b96f8c2740cff860cd
Mingxun Wang <mwang87@gmail.com>
2024-01-12 21:55:31 -0800

    updating

--- pages/dataset_usi.py
+++ pages/dataset_usi.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import requests
+import requests_cache
+
+requests_cache.install_cache('dataset_usi_cache', backend='sqlite')
 
 # Write the page label
 st.set_page_config(
     page_title="Dataset USI usage",
 )
+
 
 # Enter all the datasets accessions in textarea
 st.write("Enter all the datasets")
@@ -31,6 +35,7 @@ for dataset_accession in datasets:
             usi_dict = {}
             usi_dict["usi"] = usi
             usi_dict["dataset"] = dataset_accession
+            usi_dict["filename"] = filename
 
             all_usis.append(usi_dict)
     except:
@@ -41,3 +46,60 @@ import pandas as pd
 df = pd.DataFrame(all_usis)
 
 st.write(df)
+
+# Lets see about filtering by extension
+st.write("Filtering by extension")
+
+# get extension for filename column
+df["extension"] = df["filename"].apply(lambda x: x.split(".")[-1])
+
+# get unique extensions
+unique_extensions = df["extension"].unique()
+
+# Make a multiple selection to filter by extension
+selected_extensions = st.multiselect("Select Extensions", unique_extensions, unique_extensions)
+
+# Filter by extension
+df = df[df["extension"].isin(selected_extensions)]
+
+st.write(df)
+
+# Making an option to remove potential duplications with precidence on dataset collection
+
+# grouping files per dataset
+grouped_df = df.groupby("dataset")
+
+output_df_list = []
+# for each dataset, lets see which collections are present
+for dataset, files_df in grouped_df:
+    # get all the collections
+    collections = files_df["filename"].apply(lambda x: x.split("/")[0]).unique()
+
+    if "ccms_peak" in collections:
+        # filter to only ccms_peak
+        filtered_df = files_df[files_df["filename"].apply(lambda x: x.split("/")[0]) == "ccms_peak"]
+
+        output_df_list.append(filtered_df)
+
+    elif "peak" in collections:
+        # filter to only peak
+        filtered_df = files_df[files_df["filename"].apply(lambda x: x.split("/")[0]) == "peak"]
+
+        output_df_list.append(filtered_df)
+    
+    elif "raw" in collections:
+        # filter to only raw
+        filtered_df = files_df[files_df["filename"].apply(lambda x: x.split("/")[0]) == "raw"]
+
+        output_df_list.append(filtered_df)
+    
+    else:
+        output_df_list.append(filtered_df)
+
+# Concatenating all the dataframes
+
+final_df = pd.concat(output_df_list)
+
+st.write("Filtered DF with precidence on dataset collection, from {} to {}".format(len(df), len(final_df)))
+st.write(final_df)
+
